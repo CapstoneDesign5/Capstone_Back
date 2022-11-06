15600e1fe0ad0f9105f0299304f3de59894b9706
Sun Young Bae <tjsdud6677@gmail.com>
2022-11-06 19:15:12 +0900

    add medicine_time delete

--- routes/medicine_time.js
+++ routes/medicine_time.js
@@ -25,4 +25,27 @@ router.post('/input',(req,res)=>{
     }
 });
 
+router.post('/delete',(req,res)=>{
+    if(req.session.is_logined == true) {
+        const body = req.body;
+        const RRN = body.RRN; //해당 사용자의 주민등록번호
+        const time = body.time; //약 복용 시간
+        const date = body.date; //약 복용 날짜
+
+        client.query('select * from medicine_time where RRN=? and time=? and date=?',[RRN,time,date],(err,data)=>{
+            if(data.length != 0){ 
+                console.log('복용 시간이 삭제되었습니다.');
+                client.query('delete from medicine_time where RRN=? and time=? and date=?',[
+                    RRN, time, date
+                ]);
+            }else{  //중복된 데이터 값은 입력 불가
+                console.log('데이터를 찾지 못했습니다.');
+                res.send('데이터를 찾지 못했습니다.');
+            }
+        });
+    }else{
+        res.send('로그인이 필요합니다.');
+    }
+});
+
 module.exports = router;
